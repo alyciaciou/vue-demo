534c5674b4bb60b3bd8faa3f854a655865973018
Alycia <qw159321@gmail.com>
2022-09-21 19:55:51 +0800

    new project

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Card></Card>
   </div>
 </template>
+
+<script>
+import Card from '@/components/BootstrapCard.vue'
+
+export default {
+  components: {
+    Card
+  }
+}
+</script>
